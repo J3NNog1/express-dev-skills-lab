64f0bcb4283c0db2fa8d99d758eb81b4a895185c
Jenn <jenniferjuarezjj@gmail.com>
2022-09-21 20:32:45 -0400

    create controller data

--- routes/skills.js
+++ routes/skills.js
@@ -1,8 +1,15 @@
 import { Router } from 'express'
 import { skills } from '../data/skill-data.js'
+import * as skillsCtrl from '../controllers/skills.js'
+
+
 const router = Router()
 
+
+
 /* GET skills listing. */
+router.get('/', skillsCtrl.index)
+
 router.get('/', function(req, res) {
   res.render('skills/index', {
     skills: skills
