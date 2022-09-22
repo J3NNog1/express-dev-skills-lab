c3aaacbd9ede15051166a569d6a072d1fedda80a
Jenn <jenniferjuarezjj@gmail.com>
2022-09-21 20:22:01 -0400

    route, data, views resourse

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'HTML', done: true, _id: 125223},
+  {text: 'CSS', done: false, _id: 127904},
+  {text: 'JAVASCRIPT', done: false, _id: 139608},
+]
+
+export { 
+	skills
+}
---
+++ routes/skills.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import { skills } from '../data/skill-data.js'
+const router = Router()
+
+/* GET skills listing. */
+router.get('/', function(req, res) {
+  res.render('skills/index', {
+    skills: skills
+  })
+})
+
+export {
+  router
+}
